9bb9375b13c21bc514cb25c32cd5844880f2fc2a
ofekshmu <ofek.shmuel1@gmail.com>
2022-04-18 20:22:50 +0300

    Graph drawn, data changed to probability

--- Tic Tac Toe/Main.py
+++ Tic Tac Toe/Main.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 PLAYER1 = 1
 PLAYER2 = -1
@@ -78,15 +79,48 @@ simulations count: {n_simulations}, board size: {boardSize}
 \tPlayer1 won {stats[PLAYER1]} times
 \tPlayer2 won {stats[PLAYER2]} times
 \tTie has accured {stats[TIE]} times\n""")
+
+    return stats
          
+def plot_Data(lst, sim_count, initial_board):
+    line_1, line_2, line_3 = [], [], []
+    n = len(lst)
+    for d in lst:
+        line_1.append(d[PLAYER1]/sim_count)
+        line_2.append(d[PLAYER2]/sim_count)
+        line_3.append(d[TIE]/sim_count)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(line_1, color = 'green', label = 'Player 1 wins')
+    ax.plot(line_2, color = 'red', label = 'Player 2 wins')
+    ax.plot(line_3, color = 'blue', label = 'Ties')
+    ax.legend(loc = 'upper left')
+    
+    fig.suptitle('Probability as a function of Board Size', fontsize=12)
+    ax.set_xlabel("Board size")
+    ax.set_ylabel("Probability")
+
+    ax.set_ylim([0,1])
+    x_lst = list(range(n))
+    ax.set_xticks(x_lst)
+    ax.set_xticklabels([x + initial_board for x in x_lst])
+    plt.show()
 
 if __name__ == "__main__":
-    for size in range(3,16):
-        simulate(n_simulations=5000, boardSize=size)
+    data_lst = []
+    n_simulations = 5000
+    initial_board = 3
+    last_board = 15
+    for size in range(initial_board, last_board + 1):
+        data = simulate(n_simulations, size)
+        data_lst.append(data)
+
+    #data_lst.append(simulate(n_simulations=5000, boardSize=25))
+    #data_lst.append(simulate(n_simulations=5000, boardSize=50))
     
-    simulate(n_simulations=5000, boardSize=25)
-    simulate(n_simulations=5000, boardSize=50)
-    
+    plot_Data(data_lst, n_simulations, initial_board)
+
     #simulate(n_simulations=5000000, boardSize=25)
     #simulate(n_simulations=5000000, boardSize=50)
 

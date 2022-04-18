3c9e6ea2db8cbf44558c0f40c956fd742bb3c759
ofekshmu <ofek.shmuel1@gmail.com>
2022-04-18 15:31:46 +0300

    First Commit - V1 (project Finished)

---
+++ Main.py
@@ -0,0 +1,102 @@
+import random
+
+# None variable indicates no ladder or snake in this index
+NONE = -1
+
+def build_board():
+    return [[NONE,80,NONE,NONE,NONE,75,NONE,NONE,88,NONE],
+             [NONE,NONE,NONE,NONE,NONE,NONE,94,NONE,NONE,NONE],
+             [NONE,NONE,98,NONE,NONE,NONE,53,NONE,NONE,91],
+             [NONE,19,NONE,60,NONE,NONE,NONE,NONE,NONE,NONE],
+             [NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,67],
+             [NONE,NONE,NONE,NONE,NONE,25,NONE,NONE,11,NONE],
+             [NONE,NONE,NONE,NONE,44,NONE,NONE,NONE,NONE,NONE],
+             [42,NONE,NONE,NONE,NONE,NONE,NONE,84,NONE,NONE],
+             [NONE,NONE,NONE,NONE,6,26,NONE,NONE,NONE,NONE],
+             [NONE,38,NONE,NONE,NONE,NONE,14,31,NONE,NONE]]
+
+def roll_dice():
+    """
+    Returns a number on a dice
+    """
+    return random.randint(1,6)
+
+def check_sum(outcome, position):
+    """
+    @outcome - dice roll - a number in [1,6]
+    @position - the players position [1,100]
+    returns True if outcome equalls to position's sum of digits, False otherwise
+    """
+    sum_digits = position // 100 % 10 + position // 10 % 10  + position % 10
+    if outcome == sum_digits:
+        return True
+    return False
+
+def  get_board_value(board, position):
+    """
+    @board : game board
+    @position : player position
+    returns the value corresponding to the position of the player
+    """
+    ones = position % 10
+    tens = position // 10 % 10
+    # deciding columns - j
+    if ((position - 1) // 10 % 10 ) % 2 == 0 : # even
+        if ones == 0:
+            j = 9
+        else:
+            j = ones - 1
+    else: # odd
+        if ones == 0:
+            j = 0
+        else:
+            j = 10 - ones    
+    # deciding rows - i   
+    if ones == 0 :
+        i = 9 - (tens - 1)
+    else:
+        i = 9 - tens
+    
+    return board[i][j]
+
+def game_Loop(board):
+    
+    dice_counter = 0
+
+    position = 1
+    while( True ):
+        dice_counter += 1 # dice_counter = dice_counter + 1
+        outcome = roll_dice()
+        position = position + outcome
+        # Ending condition 
+        
+        if position > 100:
+            extra_steps =  position - 100
+            position = 100 - extra_steps
+
+        if check_sum(outcome, position) :
+            position = position // 2
+
+        if position == 100:
+            #print("Game Finished")
+            return dice_counter
+        
+        value = get_board_value(board, position)
+        if value != NONE :
+            position = value
+
+        #print(f"dice: {outcome}, position: {position}, value: {value}, cond: {check_sum(outcome, position)}")
+
+
+
+if __name__ == "__main__":
+    board = build_board()
+
+    n_simulations = 5000
+    sum = 0
+    
+    for i in range(n_simulations):
+        dice_counter = game_Loop(board)
+        sum += dice_counter
+    
+    print(f"The mean is {sum/n_simulations}")
---
+++ test_sample.py
@@ -0,0 +1,47 @@
+import Main
+
+def tests_board():
+    b = Main.build_board()
+    assert Main.get_board_value(b, 1) == -1
+    assert Main.get_board_value(b, 2) == 38
+    assert Main.get_board_value(b, 3) == -1
+    assert Main.get_board_value(b, 4) == -1
+    assert Main.get_board_value(b, 5) == -1
+    assert Main.get_board_value(b, 6) == -1
+    assert Main.get_board_value(b, 7) == 14
+    assert Main.get_board_value(b, 8) == 31
+    assert Main.get_board_value(b, 9) == -1
+    assert Main.get_board_value(b, 10) == -1
+    assert Main.get_board_value(b, 11) == -1
+    assert Main.get_board_value(b, 12) == -1
+    assert Main.get_board_value(b, 13) == -1
+    assert Main.get_board_value(b, 14) == -1
+    assert Main.get_board_value(b, 15) == 26
+    assert Main.get_board_value(b, 16) == 6
+    assert Main.get_board_value(b, 17) == -1
+    assert Main.get_board_value(b, 18) == -1
+    assert Main.get_board_value(b, 19) == -1
+    assert Main.get_board_value(b, 20) == -1
+    assert Main.get_board_value(b, 46) == 25
+    assert Main.get_board_value(b, 90) == -1
+    assert Main.get_board_value(b, 91) == -1
+    assert Main.get_board_value(b, 92) == 88
+    assert Main.get_board_value(b, 93) == -1
+    assert Main.get_board_value(b, 94) == -1
+    assert Main.get_board_value(b, 95) == 75
+    assert Main.get_board_value(b, 96) == -1
+    assert Main.get_board_value(b, 97) == -1
+    assert Main.get_board_value(b, 98) == -1
+    assert Main.get_board_value(b, 99) == 80
+    
+
+def tests_check_sum():
+    for i in range(1,7):
+        for j in range(1,101):
+            cond = i == (j // 100 + j // 10 % 10 + j % 10)
+            assert Main.check_sum(i, j) == cond
+
+
+if __name__ == "__main__":
+    tests_board()
+    tests_check_sum()
